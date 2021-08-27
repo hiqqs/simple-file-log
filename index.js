566398a8f0f0a90dba7f87bb65a620818c02dbe9
John Ahigian <jahigian@arkpes.com>
2021-08-27 17:29:19 -0400

    changing to append

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const require = createRequire(import.meta.url);
 const fs = require('fs-extra');
 
 export function simpleLog (filepath, msg) {
-    fs.writeFile(filepath, msg);
+    fs.appendFile(filepath, msg);
 };
 
 export function simpleLogSync (filepath, msg) {
-    fs.writeFileSync(filepath, msg);
+    fs.appendFile(filepath, msg);
 };
