6dffcd2c38b5daa0ad446af3f1b318c3f8bf4626
John Ahigian <jahigian@arkpes.com>
2021-08-27 17:16:59 -0400

    updates

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import { simpleLog, simpleLogSync } from './utils.js'
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const fs = require('fs-extra');
+
+export function simpleLog (filepath, msg) {
+    fs.writeFile(filepath, msg);
+};
+
+export function simpleLogSync (filepath, msg) {
+    fs.writeFileSync(filepath, msg);
+};
+
 simpleLog('logs/simple-log.txt', 'testing simple log');
 simpleLogSync('logs/simple-log-sync.txt', 'testing simple log');
