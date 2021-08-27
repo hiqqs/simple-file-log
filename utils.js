13df7c9fcd2f364885411abae45c28292a0f2e95
John Ahigian <jahigian@arkpes.com>
2021-08-27 15:28:28 -0400

    initial commit of simplelog

---
+++ utils.js
@@ -0,0 +1,11 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const fs = require('fs-extra');
+
+export function simpleLog(filepath, msg) {
+    fs.writeFile(filepath, msg);
+}
+
+export function simpleLogSync(filepath, msg) {
+    fs.writeFileSync(filepath, msg);
+}
